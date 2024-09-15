e95ba1eeaa043dd387e2b49bd4e9c0c280233c85
Anbudurai S <77448721+Anbudurai@users.noreply.github.com>
2024-09-15 17:36:19 +0530

    Fixed Secret

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIA2UC3BKI2ZCHQNFUM"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
   # Configuration options
 }
